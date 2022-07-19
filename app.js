691787fb8448be3cd48aba8260b142b82322a8e9
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-07-19 20:03:59 +0700

    fix typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./model/campground')
+const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-practice')
     .then(() => {
