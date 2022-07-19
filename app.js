970dd31c8550dac36fda05c830ed5180baa9ecfa
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-07-19 17:19:35 +0700

    Test database connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./model/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp-practice')
+    .then(() => {
+        console.log('Database connected')
+    })
+    .catch(err => {
+        console.log('connection error')
+    })
+mongoose.connection.on('error', console.error.bind(console, "connection error:"));
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +22,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
+    await camp.save();
+    res.send(camp);
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
