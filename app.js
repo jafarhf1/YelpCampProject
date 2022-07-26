42cf87ca584db4771118ec6aed92c5fca20b382b
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-07-26 22:41:59 +0700

    Install ejs-mate and use boilerplate layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -15,7 +16,7 @@ mongoose.connection.on('error', console.error.bind(console, "connection error:")
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
