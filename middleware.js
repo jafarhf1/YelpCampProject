deb4151863053adc9342ef872036a0d853c129d5
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-08-05 23:23:36 +0700

    More reviews authorization

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const ExpressError = require('./utils/ExpressError');
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -39,4 +40,14 @@ module.exports.validateReview = (req, res, next) => {
     } else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (review && !review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,7 +27,13 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    //console.log(JSON.stringify(campground, null, 4))//Just for checking if it the populate works
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds') //res.redirect doesn't end the execution of the line below it so we need to add return
