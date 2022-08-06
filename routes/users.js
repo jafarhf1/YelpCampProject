bce18f15f3b9bb7123d5bb0b2c251de40471406b
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-08-06 11:45:07 +0700

    A fancy way to restructure routes

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,16 @@ const User = require('../models/user');
 const users = require('../controllers/users');
 const passport = require('passport');
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-router.post('/register', catchAsync(users.register))
-
-router.get('/login', users.renderLogin)
-
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true, failureRedirect: '/login',
-    keepSessionInfo: true //add this option because the new passport version will remove session after login and logout so we don't have previous session information
-}), users.login)
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true, failureRedirect: '/login',
+        keepSessionInfo: true //add this option because the new passport version will remove session after login and logout so we don't have previous session information
+    }), users.login)
 
 router.get('/logout', users.logout);
 
