f7c7fe81e163ee4c1adfc5186eef8d7c229e2d9f
Ja'far Hilmy F <jafarhf97@gmail.com>
2022-08-07 13:39:01 +0700

    Customizing map popup and change style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
     projection: 'globe' // display the map as a 3D globe
@@ -9,6 +9,12 @@ const map = new mapboxgl.Map({
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
     .addTo(map)
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
